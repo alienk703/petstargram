926b3325aeb2a187705895a75387306a0ca04dc0
alienk703 <alienk703@gmail.com>
2014-09-08 12:08:32 -0400

    initialized

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,8 @@
+require “instagram”
+Instagram.configure do |config|
+config.client_id = b0468296c0cd4d93924c161d62ec2e5c
+config.access_token = 1490612062.b046829.6514d84bcc9b4f9c96241b4458def16e
+end
+
+
+
